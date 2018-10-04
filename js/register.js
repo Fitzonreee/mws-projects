372029f63896ba094bc524b46578f4e5bae33893
Fitz - iMac <kevin.fitzhenry@createthenext.com>
2018-10-03 20:19:17 -0700

    Moved sw.js out to / instead of in js/ folder

--- js/register.js
+++ js/register.js
@@ -1,12 +1,11 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('js/sw.js').then(
+    navigator.serviceWorker.register('sw.js').then(
         function(registration) {
             console.log("Successfully registered ServiceWorker");
         }
     ).catch(
         function(error) {
             console.log("Error registering ServiceWorker", error);
-            
         }
     )
 }
