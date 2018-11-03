c02d96664115dd1fd03c75714995b1092bd2fd52
Fitzonreee <kevin.fitzhenry@createthenext.com>
2018-11-03 13:42:42 -0700

    npm installed idb library, created dbpromise file and imported idb

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import DBHelper from './dbhelper';
+// import './register-sw';
 
 let restaurants,
   neighborhoods,
--- src/js/restaurant_info.js
+++ src/js/restaurant_info.js
@@ -1,4 +1,5 @@
 import DBHelper from './dbhelper';
+// import './register-sw';
 
 let restaurant;
 var newMap;
