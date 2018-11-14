8740010da951f7db29d6d786282bdb8cf5f7af5d
Fitzonreee <kevin.fitzhenry@createthenext.com>
2018-11-14 14:34:38 -0800

    Changed idb.open back to original 2 cases

--- src/js/dbpromise.js
+++ src/js/dbpromise.js
@@ -1,31 +1,14 @@
 import idb from 'idb';
 
 const dbPromise = {
- // creation and updating of database happens here.
- db: idb.open('restaurant-reviews-db', 2, function (upgradeDb) {
-   switch (upgradeDb.oldVersion) {
-     case 0:
-       upgradeDb.createObjectStore('restaurants', { keyPath: 'id' });
-     case 1:
-      upgradeDb.createObjectStore('reviews', { keyPath: 'id'})
-        .createIndex('restaurant_id', 'restaurant_id');
-        
-    //  case 2:
-    //     const putRequestStore = upgradeDb.createObjectStore('syncFavorites', {autoIncrement: true});
-        
-        // Using the restaurant id as key will allow me to easily update local iDB data,
-        // remove successful put requests, and even handle last chance Background Syncs.
-        // Another plus, is that if the user toggles the favorite button
-        // more than once while offline, only the latest action will be synced, saving requests.
-  
-      // Store an object instead of a URL: {restaurant_id: id, url: "url"}
-        
-      // const syncFavoriteStore = upgradeDb.createObjectStore('syncFavorites', {keyPath: 'restaurant_id'});
-      
-      // case 3:
-      //   const offlineReviewStore = upgradeDb.createObjectStore('offlineReviews', {keyPath: 'id', autoIncrement: true});
-      //   offlineReviewStore.createIndex('restaurant_id', 'restaurant_id');
-   
+  // creation and updating of database happens here.
+  db: idb.open('restaurant-reviews-db', 2, function (upgradeDb) {
+    switch (upgradeDb.oldVersion) {
+      case 0:
+        upgradeDb.createObjectStore('restaurants', { keyPath: 'id' });
+      case 1:
+       upgradeDb.createObjectStore('reviews', { keyPath: 'id'})
+         .createIndex('restaurant_id', 'restaurant_id');
     }
   }),
 
