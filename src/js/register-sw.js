3ff27961fbf08f6b90b6fbc9549082de83f6a2c9
Fitzonreee <kevin.fitzhenry@createthenext.com>
2018-11-28 15:51:40 -0800

    Added check for backgroundSync in register-sw.js

--- src/js/register-sw.js
+++ src/js/register-sw.js
@@ -1,11 +1,21 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js').then(
-        function(registration) {
-            console.log("Successfully registered ServiceWorker");
-        }
-    ).catch(
-        function(error) {
-            console.log("Error registering ServiceWorker", error);
-        }
-    )
-}
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('sw.js').then(
+            function(registration) {
+                
+                console.log("Successfully registered ServiceWorker!");
+                
+                if ('sync' in registration) {
+                    console.log("BackgroundSync is supported!");
+                }
+            
+            }
+        ).catch(
+            function(error) {
+                console.log("Error registering ServiceWorker", error);
+            }
+        )
+    }
+});
